37b40ca4c11a6aa758494250a00b3884efa5152a
martinsllm <lucaslm2010@gmail.com>
2025-04-10 20:46:50 -0300

    Using permissions decorators

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rolepermissions.decorators import has_permission_decorator
 
+@has_permission_decorator('cadastrar_vendedor')
 def cadastrar_vendedor(request):
     return HttpResponse('Teste')
